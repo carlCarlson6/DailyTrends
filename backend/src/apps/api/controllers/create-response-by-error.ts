b966e65a8f5c23c6140d59947929c9de958fec78
Carlos Acitores Deval <carlos.acitores@colba.es>
2021-12-08 10:25:49 +0100

    test

--- backend/src/apps/api/controllers/create-response-by-error.ts
+++ backend/src/apps/api/controllers/create-response-by-error.ts
@@ -7,10 +7,12 @@ export const createResponseByError = (response: Response, error: unknown): Respo
         
     if (e instanceof FeedNotFoundError) {
         response.status(404).send({error: e.message});
-    } else if(e instanceof RepositoryTransactionError) {
-        response.status(500).send({error: e.message})
-    } else {
+    } 
+    else if (e instanceof RepositoryTransactionError) {
+        console.log("here");
         response.status(500).send({error: e.message});
+    } else {
+        response.status(500).send({error: `an uncontrolled error happen - ${e.message}`});
     }
     
     return response;
--- backend/test/apps/api/express-feeds-controller.test.ts
+++ backend/test/apps/api/express-feeds-controller.test.ts
@@ -1,72 +1,222 @@
-import { Request, Response } from "express";
+import e, { Request, Response } from "express";
 import { It, Mock, Times } from "moq.ts";
-import { ExpressFeedsController } from "../../../src/apps/api/express-feeds-controller";
+import { ExpressFeedsController } from "../../../src/apps/api/controllers/express-feeds-controller";
 import { IFeedCrudService } from "../../../src/feeds/application/abstractions/feed-crud-service.interface";
-import { IFeedUpdater } from "../../../src/feeds/application/abstractions/feed-updater.interface";
+import { CreateFeedCommand } from "../../../src/feeds/application/messages/create-feed-command";
 import { Feed } from "../../../src/feeds/domain/entities/feed";
 import { FeedNotFoundError } from "../../../src/feeds/domain/errors/feed-not-found-error";
+import { RepositoryTransactionError } from "../../../src/feeds/domain/errors/repository-transaction-error";
 import { Logger } from "../../../src/feeds/domain/services/logger";
 
 describe("Express Feeds Controller", () => {
     const feedCrudServiceMock = new Mock<IFeedCrudService>();
-    const feedUpdaterMock = new Mock<IFeedUpdater>();
     const loggerMock = new Mock<Logger>();
-    const controller = new ExpressFeedsController(feedCrudServiceMock.object(), feedUpdaterMock.object(), loggerMock.object());
+    const controller = new ExpressFeedsController(feedCrudServiceMock.object(), loggerMock.object());
     
+    const buildRequestResponseMocks = () => ({
+        requestMock: new Mock<Request>(),
+        responseMock: new Mock<Response>(),
+    });
+
     beforeEach(() => {
         loggerMock.setup(l => l.LogInfo(It.IsAny<string>(), It.IsAny<string[]>())).returns();
         loggerMock.setup(l => l.LogWarning(It.IsAny<string>(), It.IsAny<string[]>())).returns();
         loggerMock.setup(l => l.LogError(It.IsAny<string>(), It.IsAny<string[]>())).returns();
     });
 
-    describe("given an id not correspond to any feed annd feedCrudService throws FeedNotFoundError", () => {
-        it("when GetById then the controller should return 404 response code", async () => {
-            const feedId = "some-id";
-            feedCrudServiceMock.setup(fcs => fcs.Read(feedId)).throws(new FeedNotFoundError(feedId));
+    describe("given a succesfull request", () => {
+        it("when Get then 200 status code is returned", async () => {
+            feedCrudServiceMock.setup(fc => fc.ReadAll()).returns(Promise.resolve(It.IsAny<Feed[]>()));
 
-            const requestMock = new Mock<Request>();
-            requestMock.setup(r => r.params).returns({id: feedId});
+            const {requestMock, responseMock} = buildRequestResponseMocks();
+            responseMock.setup(r => r.status(200)).returns(responseMock.object());
+            responseMock.setup(r => r.send(It.IsAny<Feed[]>())).returns(responseMock.object());
 
-            const responseMock = new Mock<Response>();
-            responseMock.setup(r => r.status(404)).returns(responseMock.object());
-            responseMock.setup(r => r.send({})).returns(responseMock.object());
+            await controller.Get(requestMock.object(), responseMock.object());
+
+            responseMock.verify(r => r.status(200), Times.Once());
+        });
+
+        it("when GetById then 200 status code is returned", async () => {
+            feedCrudServiceMock.setup(fc => fc.Read(It.IsAny<string>())).returns(Promise.resolve(It.IsAny<Feed>()));
+
+            const {requestMock, responseMock} = buildRequestResponseMocks();
+            requestMock.setup(r => r.params).returns({id: It.IsAny<string>()});
+            responseMock.setup(r => r.status(200)).returns(responseMock.object());
+            responseMock.setup(r => r.send(It.IsAny<Feed[]>())).returns(responseMock.object());
 
             await controller.GetById(requestMock.object(), responseMock.object());
 
-            responseMock.verify(r => r.status(404), Times.Once());
+            responseMock.verify(r => r.status(200), Times.Once());
         });
 
-        it("when Put then the controller should return 404 response code", async () => {
-            const feedId = "some-id";
-            const feed = new Feed(feedId, "", "", "", "", "");
-            feedCrudServiceMock.setup(fcs => fcs.Update(feed)).throws(new FeedNotFoundError(feedId));
+        it("when Post then 201 status code is returned", async () => {
+            feedCrudServiceMock.setup(fc => fc.Create(It.IsAny<CreateFeedCommand>())).returns(Promise.resolve(It.IsAny<Feed>()));
 
-            const requestMock = new Mock<Request>();
-            requestMock.setup(r => r.body).returns(feed);
+            const {requestMock, responseMock} = buildRequestResponseMocks();
+            requestMock.setup(r => r.body).returns(It.IsAny<CreateFeedCommand>());
+            responseMock.setup(r => r.status(201)).returns(responseMock.object());
+            responseMock.setup(r => r.send(It.IsAny<Feed>())).returns(responseMock.object());
 
-            const responseMock = new Mock<Response>();
-            responseMock.setup(r => r.status(404)).returns(responseMock.object());
-            responseMock.setup(r => r.send({})).returns(responseMock.object());
+            await controller.Post(requestMock.object(), responseMock.object());
+
+            responseMock.verify(r => r.status(201), Times.Once());
+        });
+
+        it("when Put then 204 status code is returned", async () => {
+            feedCrudServiceMock.setup(fc => fc.Update(It.IsAny<Feed>())).returns(Promise.resolve());
+
+            const {requestMock, responseMock} = buildRequestResponseMocks();
+            requestMock.setup(r => r.body).returns(It.IsAny<Feed>());
+            responseMock.setup(r => r.status(204)).returns(responseMock.object());
+            responseMock.setup(r => r.send()).returns(responseMock.object());
 
             await controller.Put(requestMock.object(), responseMock.object());
 
-            responseMock.verify(r => r.status(404), Times.Once());
+            responseMock.verify(r => r.status(204), Times.Once());
         });
 
-        it("when Delete then the controller should return 404 response code", async () => {
-            const feedId = "some-id";
-            feedCrudServiceMock.setup(fcs => fcs.Delete(feedId)).throws(new FeedNotFoundError(feedId));
+        it("when Delete then 204 status code is returned", async () => {
+            feedCrudServiceMock.setup(fc => fc.Delete(It.IsAny<string>())).returns(Promise.resolve());
 
-            const requestMock = new Mock<Request>();
-            requestMock.setup(r => r.params).returns({id: feedId});
-
-            const responseMock = new Mock<Response>();
-            responseMock.setup(r => r.status(404)).returns(responseMock.object());
-            responseMock.setup(r => r.send({})).returns(responseMock.object());
+            const {requestMock, responseMock} = buildRequestResponseMocks();
+            requestMock.setup(r => r.params).returns({id: It.IsAny<string>()});
+            responseMock.setup(r => r.status(204)).returns(responseMock.object());
+            responseMock.setup(r => r.send()).returns(responseMock.object());
 
             await controller.Delete(requestMock.object(), responseMock.object());
 
-            responseMock.verify(r => r.status(404), Times.Once());
-        })
+            responseMock.verify(r => r.status(204), Times.Once());
+        });
+
+    });
+
+    describe("given a failed request", () => {
+        describe("and a RepositoryTransactionError is thrown", () => {
+            const setupResponseMock = (responseMock: Mock<Response>, error: RepositoryTransactionError): Mock<Response> => {
+                responseMock.setup(r => r.status(500)).returns(responseMock.object());
+                responseMock.setup(r => r.send(It.Is<{error: string}>(e => e.error === error.message))).returns(responseMock.object());
+                return responseMock
+            };
+            const checkRepositoryTransactionError = (responseMock: Mock<Response>, error: RepositoryTransactionError) => {
+                responseMock.verify(r => r.status(500), Times.Once());
+                responseMock.verify(r => r.send(It.Is<{error: string}>(e => e.error === error.message)), Times.Once());
+            };
+
+            it("when Get then 500 status code is returned", async () => {
+                const error = new RepositoryTransactionError(It.IsAny<string>());
+                feedCrudServiceMock.setup(fc => fc.ReadAll()).throws(error);
+            
+                let {requestMock, responseMock} = buildRequestResponseMocks();
+                responseMock = setupResponseMock(responseMock, error);
+
+                await controller.Get(requestMock.object(), responseMock.object());
+    
+                checkRepositoryTransactionError(responseMock, error);
+            });
+
+            it("when GetById then 500 status code is returned", async () => {
+                const error = new RepositoryTransactionError(It.IsAny<string>());
+                feedCrudServiceMock.setup(fc => fc.Read(It.IsAny<string>())).throws(error);
+            
+                let {requestMock, responseMock} = buildRequestResponseMocks();
+                requestMock.setup(r => r.params).returns({id: It.IsAny<string>()});
+                responseMock = setupResponseMock(responseMock, error);
+
+                await controller.GetById(requestMock.object(), responseMock.object());
+    
+                checkRepositoryTransactionError(responseMock, error);
+            });
+
+            it("when Post then 500 status code is returned", async () => {
+                const error = new RepositoryTransactionError(It.IsAny<string>());
+                feedCrudServiceMock.setup(fc => fc.Create(It.IsAny<CreateFeedCommand>())).throws(error);
+
+                let {requestMock, responseMock} = buildRequestResponseMocks();
+                requestMock.setup(r => r.body).returns(It.IsAny<CreateFeedCommand>());
+                responseMock = setupResponseMock(responseMock, error);
+
+                await controller.Post(requestMock.object(), responseMock.object());
+    
+                checkRepositoryTransactionError(responseMock, error);
+            });
+
+            it("when Put then 500 status code is returned", async () => {
+                const error = new RepositoryTransactionError(It.IsAny<string>());
+                feedCrudServiceMock.setup(fc => fc.Update(It.IsAny<Feed>())).throws(error);
+
+                let {requestMock, responseMock} = buildRequestResponseMocks();
+                requestMock.setup(r => r.body).returns(It.IsAny<Feed>());
+                responseMock = setupResponseMock(responseMock, error);
+
+                await controller.Put(requestMock.object(), responseMock.object());
+    
+                checkRepositoryTransactionError(responseMock, error);
+            });
+
+            it("when Delete then 500 status code is returned", async () => {
+                const error = new RepositoryTransactionError(It.IsAny<string>());
+                feedCrudServiceMock.setup(fc => fc.Delete(It.IsAny<string>())).throws(error);
+
+                let {requestMock, responseMock} = buildRequestResponseMocks();
+                requestMock.setup(r => r.params).returns({id: It.IsAny<string>()});
+                responseMock = setupResponseMock(responseMock, error);
+
+                await controller.Put(requestMock.object(), responseMock.object());
+    
+                checkRepositoryTransactionError(responseMock, error);
+            });
+        });
+
+        describe("and FeedNotFoundError is thrown", () => {
+            const setupResponseMock = (responseMock: Mock<Response>, error: FeedNotFoundError): Mock<Response> => {
+                responseMock.setup(r => r.status(404)).returns(responseMock.object());
+                responseMock.setup(r => r.send(It.Is<{error: string}>(e => e.error === error.message))).returns(responseMock.object());
+                return responseMock
+            };
+            const checkFeedNotFoundError = (responseMock: Mock<Response>, error: FeedNotFoundError) => {
+                responseMock.verify(r => r.status(404), Times.Once());
+                responseMock.verify(r => r.send(It.Is<{error: string}>(e => e.error === error.message)), Times.Once());
+            };
+
+            it("when GetById then the controller should return 404 response code", async () => {
+                const error = new FeedNotFoundError(It.IsAny<string>());
+                feedCrudServiceMock.setup(fcs => fcs.Read(It.IsAny<string>())).throws(error);
+    
+                let {requestMock, responseMock} = buildRequestResponseMocks();
+                requestMock.setup(r => r.params).returns({id: It.IsAny()});
+                responseMock = setupResponseMock(responseMock, error);
+    
+                await controller.GetById(requestMock.object(), responseMock.object());
+    
+                checkFeedNotFoundError(responseMock, error);
+            });
+
+            it("when Put then the controller should return 404 response code", async () => {
+                const error = new FeedNotFoundError(It.IsAny<string>());
+                feedCrudServiceMock.setup(fcs => fcs.Update(It.IsAny<Feed>())).throws(error);
+    
+                let {requestMock, responseMock} = buildRequestResponseMocks();
+                requestMock.setup(r => r.body).returns(It.IsAny<Feed>());
+                responseMock = setupResponseMock(responseMock, error);
+    
+                await controller.Put(requestMock.object(), responseMock.object());
+    
+                checkFeedNotFoundError(responseMock, error);
+            });
+
+            it("when Delete then the controller should return 404 response code", async () => {
+                const error = new FeedNotFoundError(It.IsAny<string>());
+                feedCrudServiceMock.setup(fcs => fcs.Delete(It.IsAny<string>())).throws(error);
+    
+                let {requestMock, responseMock} = buildRequestResponseMocks();
+                requestMock.setup(r => r.params).returns({id: It.IsAny<string>()});
+                responseMock = setupResponseMock(responseMock, error);
+    
+                await controller.Delete(requestMock.object(), responseMock.object());
+    
+                checkFeedNotFoundError(responseMock, error);
+            });
+        });
     });
 });
