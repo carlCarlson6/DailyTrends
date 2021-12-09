d1c5cb34b9fb1a93ebdd6903e52a258df5f5fbcf
Carlos Acitores Deval <carlos.acitores@colba.es>
2021-12-09 21:49:28 +0100

    main components

---
+++ frontend/src/app/feeds/all-feeds/all-feeds.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AllFeedsComponent } from './all-feeds.component';
+
+describe('AllFeedsComponent', () => {
+  let component: AllFeedsComponent;
+  let fixture: ComponentFixture<AllFeedsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AllFeedsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AllFeedsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
