26c28651a727817f6d75c5544e276788ca07e6da
Carlos Acitores Deval <carlos.acitores@colba.es>
2021-12-10 17:43:20 +0100

    display today feeds in front page

--- frontend/src/app/front-page/front-page.component.ts
+++ frontend/src/app/front-page/front-page.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Feeds } from '../models/feed';
+import { FeedsService } from '../services/feeds.service';
 
 @Component({
     selector: 'app-front-page',
     templateUrl: './front-page.component.html',
     styleUrls: ['./front-page.component.scss']
 })
-export class FrontPageComponent {}
+export class FrontPageComponent implements OnInit {
+
+    feeds: Feeds = []
+
+    constructor(
+        private readonly feedsService: FeedsService
+    ) {}
+
+    ngOnInit(): void {
+        this.getFeeds();
+    }
+
+    getFeeds(): void {
+        this.feedsService.getFeedsByDate(new Date()).subscribe(data => this.feeds = data);
+    }
+
+}
