b8b5d52d93315d00e53e63f669f71c39954068f5
Carlos Acitores Deval <carlos.acitores@colba.es>
2021-12-11 13:24:46 +0100

    routes

--- frontend/src/app/routes/app-routes.ts
+++ frontend/src/app/routes/app-routes.ts
@@ -1,13 +1,13 @@
 import { Routes } from '@angular/router';
 import { AllFeedsComponent } from '../feeds/all-feeds/all-feeds.component';
 import { CreateFeedComponent } from '../feeds/create-feed/create-feed.component';
-import { FeedDatailComponent } from '../feeds/feed-datail/feed-datail.component';
+import { FeedDetailComponent } from '../feeds/feed-detail/feed-detail.component';
 import { FrontPageComponent } from '../front-page/front-page.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/today', pathMatch: 'full' },
     { path: 'today', component: FrontPageComponent },
-    { path: 'feeds/:id', component: FeedDatailComponent },
+    { path: 'feeds/:id', component: FeedDetailComponent },
     { path: 'feeds', component: AllFeedsComponent },
-    { path: 'feeds/new', component: CreateFeedComponent }
+    { path: 'add', component: CreateFeedComponent }
 ];
