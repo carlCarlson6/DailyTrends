cba844a117d06ae68a0fe12d9b12f1ac7f8a9ed6
Carlos Acitores Deval <carlos.acitores@colba.es>
2021-12-11 13:25:05 +0100

    buttons on card and handle delete

--- frontend/src/app/feeds/feed-card/feed-card.component.ts
+++ frontend/src/app/feeds/feed-card/feed-card.component.ts
@@ -1,21 +1,35 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Feed } from 'src/app/models/feed';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Feed, Feeds } from 'src/app/models/feed';
+import { FeedsService } from 'src/app/services/feeds.service';
 
 @Component({
     selector: 'app-feed-card',
     templateUrl: './feed-card.component.html'
 })
-export class FeedCardComponent  {
+export class FeedCardComponent {
     @Input() feed!: Feed;
 
+    @Input() allFeeds: Feeds = [];
+    @Output() allFeedsChange = new EventEmitter<Feeds>();
+
     private displayMaxBodyLength = 240;
+
+    constructor(
+        private readonly feedsService: FeedsService
+    ) {}
 
     displayBodyResume(): string {
         if (this.feed.body.length <= this.displayMaxBodyLength) {
             return this.feed.body;
         }
 
-        const resumeBody = this.feed.body.slice(0, 240).concat(' ...')
+        const resumeBody = this.feed.body.slice(0, 240).concat('...')
         return resumeBody;
+    }
+
+    handleDelete(): void {
+        this.feedsService.deleteFeed(this.feed).subscribe();
+        this.allFeeds = this.allFeeds.filter(f => f.id !== this.feed.id);
+        this.allFeedsChange.emit(this.allFeeds);
     }
 }
