0c81ae5e3d1d211dc0848b7f5b97405daae13dfc
Carlos Acitores Deval <carlos.acitores@colba.es>
2021-12-11 13:22:48 +0100

    create feed component

--- frontend/src/app/feeds/create-feed/create-feed.component.ts
+++ frontend/src/app/feeds/create-feed/create-feed.component.ts
@@ -1,7 +1,33 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Feed, Feeds } from 'src/app/models/feed';
+import { FeedsService } from 'src/app/services/feeds.service';
+import { Location } from '@angular/common';
 
 @Component({
     selector: 'app-create-feed',
-    templateUrl: './create-feed.component.html'
+    templateUrl: './create-feed.component.html',
+    styleUrls: ['./create-feed.component.scss']
 })
-export class CreateFeedComponent {}
+export class CreateFeedComponent {
+
+    inputFeedForm = new FormGroup({
+        title: new FormControl('', Validators.required),
+        publisher: new FormControl('', Validators.required),
+        image: new FormControl('', Validators.required),
+        source: new FormControl('', Validators.required),
+        body: new FormControl('', Validators.required),
+    });
+
+    constructor(
+        private readonly feedsService: FeedsService,
+        private location: Location
+    ) {}
+
+    onSubmit(): void {
+        const feed: Feed = this.inputFeedForm.value;
+        console.log(feed);
+        this.feedsService.postFeed(feed).subscribe();
+        this.location.back();
+    }
+}
